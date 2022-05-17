a8272fd91e750f02e670b4fc5ecdf86ec4b118d4
Kathleen <83776807+Katt27@users.noreply.github.com>
2022-05-17 17:39:55 -0400

    updated cuny folder

--- cunydesign/showMenu.js
+++ cunydesign/showMenu.js
@@ -1,5 +1,8 @@
 function showMenu() {
-  var  menu = document.getElementById("menu");
-  if  (menu.style.display==="none"){ menu.style.display="block"; }
-  else { menu.style.display="none"; }
+  var menu = document.getElementById("menu");
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
 }
